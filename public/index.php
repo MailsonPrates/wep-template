ca3bf19e51ae199ac46a1f825c4b32503f7138f7
MailsonPrates <mailsonprates@gmail.com>
2025-02-18 15:38:42 -0300

    Adiciona helpers

--- public/index.php
+++ public/index.php
@@ -13,4 +13,25 @@ if ( ENV == "DEV" ){
     ini_set('display_errors', '1');
 }
 
+/**
+ * OC Helpers
+ */
+
+ function utf8_substr($string, $offset, $length = null) {
+    if ($length === null) {
+        return iconv_substr($string, $offset, utf8_strlen($string), 'UTF-8');
+    } else {
+        return iconv_substr($string, $offset, $length, 'UTF-8');
+    }
+}
+
+function utf8_strlen($string) {
+    return mb_strlen($string);
+}
+
+function utf8_strrpos($string, $needle, $offset = 0) {
+    return mb_strrpos($string, $needle, $offset);
+}
+
+
 $app->run();

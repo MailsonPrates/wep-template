c2464669a6945ef94aea18e90a14060a4671cb4f
MailsonPrates <mailsonprates@gmail.com>
2025-02-20 17:38:33 -0300

    Refactor

--- src/Common/Templates/Main.php
+++ src/Common/Templates/Main.php
@@ -14,18 +14,17 @@ class Main implements iTemplate
      */
     public function build($route, $request, $template): string
     {
-        $use_cache_booster = ENV_DEV;
+        $view_assets = include_once(DIR_STORAGE . "/builds/view-assets.build.php");
 
         $html = $template->html([
             "title" => $route->view->title ?? "",
             "head" => [
-                ["link", ["rel" => "icon", "href" => App::assets("images/favicon.png")]],
-                ["link", ["rel" => "stylesheet", "href" => "https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap"]],
+                ["link", ["rel" => "icon", "href" => App::assets("images/favicon.png?v=3")]],
+                ["link", ["rel" => "stylesheet", "href" => "https://fonts.googleapis.com/css2?family=Jost:wght@300;400;500;700;900&display=swap"]],
                 ["link", ["rel" => "stylesheet", "href" => App::assets("css/vendor/bootstrap.css")]],
+                ["link", ["rel" => "stylesheet", "href" => App::assets("css/main.css")]],
                 ["script", ["src" => App::assets("js/vendor/jquery.js")]],
-
-                // Entry point
-                ["script", ['id' => 'main-script', "src" => App::assets("view/main.js", $use_cache_booster)]],
+                ...$view_assets
             ]
         ]);
 
